ce01f3c74136575b28471f986012b3f21ec96db8
kubila <dolaskyasin@gmail.com>
2020-01-29 21:58:10 +0300

    Second Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<h5>This is a mini MVC framework. Feel free to use and improve it. If you find any issues please contact me.</h5>
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,5 +1,5 @@
 <?php require APP_ROOT . "/views/inc/header.php"; ?>
 
 <h1 class="some"><?php echo $data["title"]; ?></h1>
-<p>This is a MVC implemented mini-PHP framework. Please feel free to improve and use it.</p>
+<p>This is a MVC implemented mini-PHP framework.</p>
 <?php require APP_ROOT . "/views/inc/footer.php"; ?>
